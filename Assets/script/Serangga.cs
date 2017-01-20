727e93ac7ba0c2b881dcd6ffb56097039e1ce8ed
Dhany Nurdiansyah <dhanyn@outlook.co.id>
2017-01-20 21:55:28 +0700

    update position serangga

---
+++ Assets/script/Serangga.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Serangga : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Time.deltaTime * -2, 0,0);
+	}
+}
---
+++ Assets/script/SpawnMusuh.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnMusuh : MonoBehaviour {
+	public GameObject serangga;
+
+	float waktu;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		waktu += Time.deltaTime;
+		if (waktu >= 1) {
+			Instantiate (serangga, transform.position, transform.rotation);
+			waktu = 0;
+		}
+	}
+}
