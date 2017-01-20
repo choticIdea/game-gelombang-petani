892933c231bce2e0efd3be1525a2a464fac7b143
Dhany Nurdiansyah <dhanyn@outlook.co.id>
2017-01-21 00:20:46 +0700

    update position serangga

--- Assets/script/Serangga.cs
+++ Assets/script/Serangga.cs
@@ -2,14 +2,35 @@
 using System.Collections;
 
 public class Serangga : MonoBehaviour {
-
+	public float kec_serangga_x = 1, kec_serangga_y = 1;
+	string zigzag = "";
+	float waktu;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Time.deltaTime * -2, 0,0);
+		waktu += Time.deltaTime;
+		if (waktu >= 1) {
+			int randomZigzag = Random.Range (0,2);
+			if(randomZigzag == 0)
+				zigzag = "naik";
+			else if(randomZigzag == 1)
+				zigzag = "turun";
+			waktu = 0;
+		}
+		if (zigzag == "naik") {
+			if(transform.position.y >= 4)
+				transform.Translate (Time.deltaTime * -2 * kec_serangga_x, 0, 0);
+			else
+				transform.Translate (Time.deltaTime * -2 * kec_serangga_x, Time.deltaTime * 2 * kec_serangga_y, 0);
+		} else if (zigzag == "turun") {
+			if(transform.position.y <= -4)
+				transform.Translate (Time.deltaTime * -2 * kec_serangga_x, 0, 0);
+			else
+				transform.Translate (Time.deltaTime * -2 * kec_serangga_x, Time.deltaTime * -2 * kec_serangga_y, 0);
+		}
 	}
 }
---
+++ Assets/script/Text.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Text : MonoBehaviour {
+	public UnityEngine.UI.Text text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "lalala";
+	}
+}
