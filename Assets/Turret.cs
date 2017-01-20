19c8b6915b143c8a21309528c4ab0b225fbd5372
Dhany Nurdiansyah <dhanyn@outlook.co.id>
2017-01-21 00:38:34 +0700

    update maul

---
+++ Assets/Turret.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+
+	// Use this for initialization
+	Vector3 rot;
+	Vector3 mousePos;
+	Vector3 pos;
+	Vector3 diff;
+	public GameObject [] weaponries; 
+	int equipped = 0; // 0,1,2
+	void Start () {
+		pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		equipped = equipped + checkMouseScrolls ();
+		if (equipped > weaponries.Length - 1)
+			equipped = 0;
+		else if (equipped < 0)
+			equipped = weaponries.Length - 1;
+		
+		rot = transform.rotation.eulerAngles;
+		rot.z = Mathf.Atan2 (mousePos.y, mousePos.x)* Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(rot);
+
+		if (Input.GetMouseButtonDown (0)) {
+			//Bad Spawning
+			mousePos.Normalize();
+			GameObject g = (GameObject) Instantiate (weaponries[equipped], transform.position, transform.rotation);
+			Wave w = g.GetComponent<Wave> ();
+			Vector2 dir = new Vector2 (0,0);
+			dir.x = mousePos.x;
+			dir.y = mousePos.y;
+			w.setSpeedDir (dir);
+		}
+	}
+	int checkMouseScrolls(){
+		if (Input.mouseScrollDelta.y > 0)
+			return 1;
+		else if (Input.mouseScrollDelta.y < 0)
+			return -1;
+		else
+			return 0;
+	}
+}
---
+++ Assets/Wave.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour {
+
+	// Use this for initialization
+	Vector2 dir;
+	public float baseSpeed = 2f;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = transform.position;
+		pos += dir*baseSpeed;
+		transform.position = pos;
+	}
+	public void setSpeedDir(Vector2 speeddir){
+		dir = speeddir;
+		Debug.Log ("Speed set !");
+	}
+	void checkOutofBoundaries(){
+	
+	}
+}
