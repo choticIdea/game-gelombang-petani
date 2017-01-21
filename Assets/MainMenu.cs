abd43fc72927e758ee48ead9d822acdbfb412850
Vinci <lana.shurtugal@gmail.com>
2017-01-21 14:17:34 +0700

    adding gamemaster stubs

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void StartGame(){
+		SceneManager.LoadScene ("game");
+	}
+	public void Tutorial(){
+		SceneManager.LoadScene ("Tutorial");
+	}
+	public void Credits(){
+		SceneManager.LoadScene ("Credits");
+	}
+	public void Exit(){
+		Application.Quit ();
+	}
+}
---
+++ Assets/script/GameMaster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+	// Use this for initialization
+	public static int playerCoins;
+	public static int playerScore;
+	public static int paddyHealth;
+	public static bool gameOver = false;
+
+	public void Init () {
+		playerCoins = 0;
+		playerScore = 0;
+		paddyHealth = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void DetermineEnemySets(){
+	
+	}
+	void SaveGame(){
+	//stubs
+	}
+	void LoadGame(){
+	//stubs
+	}
+
+}
